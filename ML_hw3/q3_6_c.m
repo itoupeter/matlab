c44d944d60b8a6bdc89782bfa7117710fd64fe5c
Liang Peng <penliang@seas.upenn.edu>
2016-10-07 09:29:58 -0400

    fix q_3_6_c.m

--- ML_hw3/q3_6_c.m
+++ ML_hw3/q3_6_c.m
@@ -3,7 +3,7 @@ load('hw3_data/data.mat');
 
 options = optimset('PlotFcns', @optimplotfval);
 w0 = [0; 0; 0];
-lambda = 8;
+lambda = 30;
 w = fminsearch(@q3func2, w0, options, X, Y, lambda);
 
 w_mle = [0.8891; -0.8260; 4.1902];
