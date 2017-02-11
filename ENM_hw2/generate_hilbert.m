1317e5dec707180aa701e4e779b728971ee63427
Liang Peng <penliang@seas.upenn.edu>
2017-02-10 22:33:20 -0500

    502 hilbert

---
+++ ENM_hw2/generate_hilbert.m
@@ -0,0 +1,14 @@
+function [A] = generate_hilbert(N, M)
+% This function generates Hilbert matrix of size N by M
+
+if ~exist('M')
+    M = N
+end
+
+B = [1 : N]'; % [1 2 ...]'
+B = repmat(B, 1, M);
+
+C = [0 : M - 1]; % [1 2 ...]
+C = repmat(C, N, 1);
+
+A = 1 ./ (B + C);
