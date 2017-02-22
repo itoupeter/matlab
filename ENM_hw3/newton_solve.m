f43f331109a9f4d950270820dd1e2282abbf5c8e
itoupeter <itoupeter@gmail.com>
2017-02-22 13:59:12 -0500

    502 newton solver

---
+++ ENM_hw3/newton_solve.m
@@ -0,0 +1,32 @@
+function [x] = newton_solve(A, b, x0)
+% function: solve for x in Ax=b using Newton's method.
+
+n = size(A, 1);
+xx = ones(n, 1);
+x = zeros(n, 1);
+ite = 0;
+tol = 0.001;
+
+if exist('x0')
+    xx = x0;
+end
+
+while 1
+    r = A * xx - b;
+    dx = direct_solve(A, -r); dx
+    x = xx + dx;
+
+    if norm(x - xx) < tol
+        break;
+    elseif norm(x) > 1e10
+        disp('Diverges.');
+        x = zeros(n, 0);
+        return;
+    else
+        xx = x;
+    end
+
+    ite = ite + 1;
+end
+
+disp(['# of iterations: ', num2str(ite)]);
