ffafe6c49638673a36f23c8926f65d61048855b7
LIANG PENG <penliang@dc1.base.seas.upenn.edu>
2017-03-01 19:31:08 -0500

    502 plot time

--- ENM_hw3/plot_time.m
+++ ENM_hw3/plot_time.m
@@ -8,21 +8,27 @@ hold on;
 loglog(ns, time_jacobi);
 loglog(ns, time_gs);
 
-x = ns(:, 30:end);
-y_direct = time_direct(:, 30:end);
-y_jacobi = time_jacobi(:, 30:end);
-y_gs = time_gs(:, 30:end);
+start = 40;
+x = ns(:, start:end);
+y_direct = time_direct(:, start:end);
+y_jacobi = time_jacobi(:, start:end);
+y_gs = time_gs(:, start:end);
 
 P1 = polyfit(log(x), log(y_direct), 1);
 P2 = polyfit(log(x), log(y_jacobi), 1);
 P3 = polyfit(log(x), log(y_gs), 1);
 
-fit_direct = P1(1) * x + P1(2);
-fit_jacobi = P2(1) * x + P2(2);
-fit_gs = P3(1) * x + P3(2);
+fit_direct = P1(1) * log(x) + P1(2);
+fit_jacobi = P2(1) * log(x) + P2(2);
+fit_gs = P3(1) * log(x) + P3(2);
 
-loglog(x, fit_direct);
-loglog(x, fit_jacobi);
-loglog(x, fit_gs);
+loglog(x, exp(fit_direct));
+loglog(x, exp(fit_jacobi));
+loglog(x, exp(fit_gs));
+
+title('Time Scaling');
+xlabel('Matrix Size');
+ylabel('Time to Solve');
+legend('Direct', 'Jacobi', 'Gauss-Seidel', 'Direct Linear Fit', 'Jacobi Linear Fit', 'Gauss-Seidel Linear Fit', 'location', 'northwest');
 
 hold off;
