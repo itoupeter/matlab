bc6a627177e7646c84eabdb4679fd0a92cfb028e
Liang Peng <penliang@seas.upenn.edu>
2017-03-24 18:08:54 -0400

    502 branch 1

--- ENM_hw4/ac_lambda.m
+++ ENM_hw4/ac_lambda.m
@@ -6,45 +6,44 @@ x = repmat(x, N, 1);
 y = x';
 
 % construct matrix A (FD of the Laplacian)
-M = (N - 2) * (N - 2); % M unknowns
-A = zeros(M, M);
+M = N * N; % M unknowns
+A = eye(M, M);
 
 sinpix = sin(pi * x);
-sinpix = reshape(sinpix(2:N - 1, 2:N - 1)', [], 1);
+sinpix = reshape(sinpix', [], 1);
 
 epsilon = 0;
 
 h_2 = (N - 1) * (N - 1);
-row_id = 0;
 
 for i = 2 : N - 1
     for j = 2 : N - 1
         % construct 1 row in 1 inner loop
-        
+
         % center
         id = grid_id(i, j, N);
         A(id, id) = -4 * h_2;
 
         % east
-        if j < N - 1
+        if j + 1 <= N
             eid = grid_id(i, j + 1, N);
             A(id, eid) = h_2;
         end
 
         % west
-        if j > 2
+        if j - 1 >= 1
             wid = grid_id(i, j - 1, N);
             A(id, wid) = h_2;
         end
 
         % north
-        if i > 2
+        if i - 1 >= 1
             nid = grid_id(i - 1, j, N);
             A(id, nid) = h_2;
         end
 
         % south
-        if i < N - 1
+        if i + 1 <= N
             sid = grid_id(i + 1, j, N);
             A(id, sid) = h_2;
         end
@@ -54,30 +53,32 @@ end
 % linear problem solution
 n = 1;
 m = 1;
-Anm = -1;
+Anm = 1;
 u = Anm .* sin(m * pi * x) .* sin(n * pi * y);
-U = reshape(u(2:N - 1, 2:N - 1)', [], 1);
-lambda = (m.^2 + n.^2) * pi.^2;
+U = reshape(u', [], 1);
 
 % advance paramter to do AC (lambda in this case)
-delta_lambda = 0.01;
+lambda = (m.^2 + n.^2) * pi.^2;
+delta_lambda = -0.1;
 
 Unorms = [];
-lambdas = lambda : delta_lambda : 60;
-for next_lambda = lambdas
+lambdas = lambda : delta_lambda : 2;
+for old_lambda = lambdas
     % dR_dU * dU_dLambda = -dR_dLambda
     % solve for dU_dLambda
     % U_k+1 = U_k + dU_dLambda * delta_lambda to get initial guess
-    dR_dU = A + diag(next_lambda * (1 + 2 * U));
+    dR_dU = A + diag(old_lambda * (1 + 2 * U));
     dR_dLambda = U .* (1 + U);
     dU_dLambda = -dR_dU \ dR_dLambda;
     U0 = U + dU_dLambda * delta_lambda;
 
     % use Newton to converge to solutionon_solve(A
-    U = newton_solve(A, next_lambda, epsilon, U0);
+    U = newton_solve(A, old_lambda + delta_lambda, epsilon, U0);
+%     plot_solution(U, x, y, N); zlim([0 1]);
+%     pause(0.2);
+    
+    % save solution norm for plotting
     Unorms = [Unorms norm(U, 2)];
-    plot_solution(U, x, y, N);
-    pause(0.1);
 end
 
 plot(lambdas, Unorms, 'rx');
--- ENM_hw4/plot_solution.m
+++ ENM_hw4/plot_solution.m
@@ -2,14 +2,13 @@ function [ dummy ] = plot_solution(U, x, y, N)
 %PLOT_SOLUTION Summary of this function goes here
 %   Detailed explanation goes here
 
-Uf = zeros(N, N);
-Uf(2:N - 1, 2:N - 1) = reshape(U, N - 2, N - 2)';
+U = reshape(U, N, N)';
 
 figure(1);
-surf(x, y, Uf);
+surf(x, y, U);
 
 figure(2);
-[C, h] = contour(x, y, Uf);
+[C, h] = contour(x, y, U);
 clabel(C, h);
 
 end
