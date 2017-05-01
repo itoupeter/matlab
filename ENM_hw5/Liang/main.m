c764650cc29ecd3ce43f6285aa4a24ea4fbacc24
Liang Peng <penliang@seas.upenn.edu>
2017-05-01 13:00:20 -0400

    tmp

--- ENM_hw5/Liang/main.m
+++ ENM_hw5/Liang/main.m
@@ -27,26 +27,43 @@ D = {@(x) k * (1 - x)^2 * (1 + x) * 0.25 / h, ...
     @(x) k * (1 - x) * (1 + x)^2 * 0.25 / h};
 
 % equation
-J = zeros(N + 1, N + 1);
-c = ones(N + 1, 1) * 0.5;
-R = zeros(N + 1, 1);
+J = zeros(N, N);
+c = ones(N, 1) * 0.5;
+R = zeros(N, 1);
 
 for e = 1 : N
     % local-to-global index mapping
     idx_global = @(x) x + e - 2;
     
     J_tmp = zeros(2, 2);
+    R_tmp = zeros(2, 1);
     
     for i = 1 : 2
         for j = 1 : 2
             j_global = idx_global(j);
             m_global = idx_global(3 - j);
             
-            J_tmp(i, j) = gsi(A{i, j}) + gsi(B{i, j}) ...
-                + c(j_global) * gsi(C{i, j})...
-                + c(m_global) * gsi(D{i, j});
+            if j_global == 0
+                cj = 1;
+            else
+                cj = c(j_global);
+            end
+            
+            if m_global == 0
+                cm = 1;
+            else
+                cm = c(m_global);
+            end
+            
+            J_tmp(i, j) = gqi(A{i, j}) + gqi(B{i, j}) ...
+                + cj * gqi(C{i, j})...
+                + cm * gqi(D{i, j});
         end
     end
     
     J(e:e + 1, e:e + 1) = J_tmp;
+    
+    for i = 1 : 2
+        
+    end
 end
